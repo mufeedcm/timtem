7bbeff939caeda951b1a02cd26a0b6cfac32bc2a
mufeedcm <muhammedmufeedcm@gmail.com>
2022-11-13 11:47:06 +0530

    test

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.{html,js}"],
+  content: [
+    "./*.{html,js}",
+    "./projects/*.{html,js}"
+  ],
   theme: {
     extend: {},
   },
