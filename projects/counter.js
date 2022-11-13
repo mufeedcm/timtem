f6cb8cfaedf7ccecdba714b6601ef52f67f23669
mufeedcm <muhammedmufeedcm@gmail.com>
2022-11-13 12:22:00 +0530

    test

---
+++ projects/counter.js
@@ -0,0 +1,7 @@
+import '../style.css'
+
+document.querySelector('#counter').innerHTML = `
+  <div class="flex justify-center">
+    <p class="bg-green-200">counter ..</p>
+  </div>
+`
---
+++ projects/randomColor.js
@@ -0,0 +1,8 @@
+import '../style.css'
+
+document.querySelector('#randomColor').innerHTML = `
+  <div class="  bg-black h-[100vh]">
+    <p class="bg-green-200">hello..</p>
+    <p class="bg-green-200">hello..</p>
+  </div>
+`
